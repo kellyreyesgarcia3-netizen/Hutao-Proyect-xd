668716a8a6985837ab081cb5a0df393caa6ce405
~🩵Miku🩵~ <kellyreyesgarcia3@gmail.com>
2025-12-03 07:42:04 -0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,84 @@
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+
+global.botnumber = ''
+
+global.owner = [
+    ["17089308726", "Propietario 👑", true],
+    //num en lid v:
+    ["17089308726", "creadorLid", true],
+    ["51993175834"],    
+    ["5218712620915"],
+    ["5351524614"]
+];
+
+global.mods = []
+global.suittag = []
+global.prems = []
+
+global.packsticker = '★彡( 𐐘ł_₴Ø₣ł₳ ₵ₕ )彡★'
+global.packname = '󠁖󠁖󠁖󠁖󠁖󠁖󠁻󠁻𝐇𝐮ⷡ𝐓ⷪ𝐚ⷮ𝐨-𝐌𝐃󠁂󠀧󠁂🈀'
+global.author = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.wm = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.titulowm = '★彡( 𐐘ł_₴Ø₣ł₳ ₵ₕ )彡★'
+global.titulowm2 = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.igfg = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.botname = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.dev = '★彡( 𐐘ł_₴Ø₣ł₳ ₵ₕ )彡★'
+global.textbot = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.gt = '𝐇𝐮ⷡ𝐓ⷪ𝐚ⷮ𝐨-𝐌𝐃󠁂󠀧󠁂🈀'
+global.namechannel = 'ɦʊȶǟօ-քʀօʏɛƈȶ'
+global.vs = 'V2.3'
+
+global.imagen1 = "https://files-furina.stellarwa.xyz/1759366152803.jpg"
+global.imagen2 = "https://files-furina.stellarwa.xyz/1763136135509.jpg"
+global.imagen3 = "https://files-furina.stellarwa.xyz/1763136134850.jpg"
+global.imagen4 = "https://files-furina.stellarwa.xyz/1763136160223.jpg"
+global.imagen5 = fs.readFileSync('./src/+18.jpg')
+global.imagen6 = "https://files-furina.stellarwa.xyz/1763136179619.jpg"
+global.imagen7 = "https://files-furina.stellarwa.xyz/1763136191624.jpg"
+global.imagen8 = "https://files-furina.stellarwa.xyz/1763136575331.jpg"
+global.imagen9 = "https://files-furina.stellarwa.xyz/1763136580625.jpg"
+global.imagen10 = "https://files-furina.stellarwa.xyz/1763136591252.jpg"
+global.miniurl = "https://files-furina.stellarwa.xyz/1763136607528.jpg"
+global.logo2 = "https://files-furina.stellarwa.xyz/1763136612880.jpg"
+global.logo3 = "https://files-furina.stellarwa.xyz/1763136612880.jpg"
+global.catalogo = "https://files-furina.stellarwa.xyz/1763136613587.jpg"
+global.logo4 = "https://files-furina.stellarwa.xyz/1763136613834.jpg"
+global.logo5 = "https://files-furina.stellarwa.xyz/1763136615365.jpg"
+global.logo7 = "https://files-furina.stellarwa.xyz/1763136615901.jpg"
+global.logo8 = "https://files-furina.stellarwa.xyz/1763136621219.jpg"
+global.rule = fs.readFileSync('./src/rule.jpg')
+global.welcome = "https://files-furina.stellarwa.xyz/1763136621896.jpg"
+global.adios = "https://files-furina.stellarwa.xyz/1763136622229.jpg"
+
+global.photoSity = [imagen8, imagen1, imagen4, imagen6]
+
+global.languaje = 'Es'
+global.nameqr = 'HuTao'
+global.sessions = 'Session/Hutao'
+global.jadi = 'Session/SubBot'
+
+global.channel = {
+channel1: "120363404567175987@newsletter",
+channel2: "120363404567175987@newsletter",
+channel3: "120363404567175987@newsletter",
+channel4: "120363404567175987@newsletter",
+channel5: "120363404567175987@newsletter"
+}
+
+global.gp4 = 'https://chat.whatsapp.com/GJiDeiFHsdeAQD0uoDBpEI?mode=hqrt3'
+global.gp1 = 'https://chat.whatsapp.com/J0HJBNlznVNIrqHgz5nGsX?mode=hqrt3'
+global.channel = 'https://whatsapp.com/channel/0029Vb6iPeUD8SE7OCpohu25'
+global.md = 'https://github.com/CheirZ'
+global.correo = 'kellyreyesgarcia3@outlook.com'
+global.cn = 'https://chat.whatsapp.com/DFsGVZPO94QJze8NIG1srE?mode=hqrt3'
+
+global.cheerio = cheerio;
+global.fs = fs;
+global.fetch = fetch;
+global.axios = axios;
+global.moment = moment;
